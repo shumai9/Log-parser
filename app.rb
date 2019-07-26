697f577fffe9bf0ec5b53386f6ad4dac9eea3535
Shumai <shumai9@github.com>
2019-07-26 06:07:23 +0100

    created a display function

--- app.rb
+++ app.rb
@@ -1,45 +1,39 @@
 require './lib/file_parser'
 require 'benchmark'
 
-
 path = "log/webserver.log"
-def app(path)
-	#parse and get count
-	#show visted pages in order (desc or asc)
-	#show uinique views in order
+
+Benchmark.bm(20) do |bm|  
+	bm.report("Create arr objs & Display:") do
+		#created array to save objects
+		parsed_data = [ 
+			visit_count = ParseUtils::PageViewsAnalizer.count_page_views(
+				ParseUtils::FileParser.parse_file(path)
+			),
+			sort_visited = ParseUtils::PageViewsAnalizer.sort_page_views(visit_count),
+			unique_visits = ParseUtils::PageViewsAnalizer.filter_unique_views(sort_visited)
+		]
+		#created method to display
+		def display_views(data_obj)
+			data_obj.map{ |k,v| puts "\r\n Visited_Page | Total_Count #{" "*20}\r\n #{k} #{" "*5}:#{" "*10}#{v}" }
+		end
+
+		parsed_data.each do |d|
+			case d
+				when visit_count
+					p "Total page visits count"			
+				when sort_visited
+					p"Ordered total page visits "		
+				when unique_visits
+					p "Unique page visits"
+				else p "It empty"				
+			end
+			display_views(d)
+			p '><'*50
+			p d
+		end
+
+	end
 end
 
-# The 20 is the width of the first column in the output.
-Benchmark.bm(20) do |bm|  
-	bm.report("var not created:") do 
-		puts "Number of visits counted \r\n #{
-			ParseUtils::PageViewsAnalizer.count_page_views(
-				ParseUtils::FileParser.parse_file(path)
-			)
-		}"
-	end
-end
-Benchmark.bm(20) do |bm|
-	bm.report("Read and parse log:") do
-		puts "Page visits sorted in decsending order \r\n #{
-			ParseUtils::PageViewsAnalizer.sort_page_views(
-				ParseUtils::PageViewsAnalizer.count_page_views(
-					ParseUtils::FileParser.parse_file(path)
-				)
-			)            
-		}"
-	end
-end
-Benchmark.bm(20) do |bm|
-	bm.report("Read and parse log:") do
-		puts "Unique page views \n #{
-			ParseUtils::PageViewsAnalizer.filter_unique_views(
-				ParseUtils::PageViewsAnalizer.sort_page_views(
-					ParseUtils::PageViewsAnalizer.count_page_views(
-						ParseUtils::FileParser.parse_file(path)
-					)
-				)
-			)
-		}"
-	end
-end
+p '@'*100
